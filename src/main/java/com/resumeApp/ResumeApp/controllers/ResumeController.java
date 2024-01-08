c9b7dfde1f13d67afb8285fba277e89886d348d1
Matej Thomka <matej.thomka.eleinst@gmail.com>
2024-01-08 22:22:15 +0100

    create and update endpoints for show and create personal details.

--- src/main/java/com/resumeApp/ResumeApp/controllers/ResumeController.java
+++ src/main/java/com/resumeApp/ResumeApp/controllers/ResumeController.java
@@ -1,14 +1,16 @@
 package com.resumeApp.ResumeApp.controllers;
 
 import com.resumeApp.ResumeApp.exceptions.ResumeAppException;
-import com.resumeApp.ResumeApp.models.User;
+import com.resumeApp.ResumeApp.models.PersonalDetails;
 import com.resumeApp.ResumeApp.models.dtos.PersonalDetailsDTO;
 import com.resumeApp.ResumeApp.services.PersonalDetailsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,16 +21,30 @@ public class ResumeController {
 
   private final PersonalDetailsService personalDetailsService;
 
-  @PostMapping("/{user}/personal_details")
-  public ResponseEntity<?> personalDetails(@PathVariable User user) {
+  @GetMapping("/{email}/show")
+  public ResponseEntity<?> show(@PathVariable String email) {
     PersonalDetailsDTO personalDetailsDTO;
 
     try {
-      personalDetailsDTO = personalDetailsService.showPersonalDetails(user);
+      personalDetailsDTO = personalDetailsService.showPersonalDetails(email);
     } catch (ResumeAppException e) {
       return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
     }
 
     return new ResponseEntity<>(personalDetailsDTO, HttpStatus.OK);
+  }
+
+  @PostMapping("/{email}/create")
+  public ResponseEntity<?> create(@PathVariable String email,
+                                  @RequestBody PersonalDetails personalDetails) {
+
+
+    try {
+      personalDetailsService.createPersonalDetails(email, personalDetails);
+    } catch (ResumeAppException e) {
+      return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    return new ResponseEntity<>("Personal details added!", HttpStatus.CREATED);
   }
 }
