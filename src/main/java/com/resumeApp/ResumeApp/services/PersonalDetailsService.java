4462df27553b1aec0cfef079bf13ddf2d135ce70
Matej Thomka <matej.thomka.eleinst@gmail.com>
2024-01-08 22:25:16 +0100

    Clear unused imports.

--- src/main/java/com/resumeApp/ResumeApp/services/PersonalDetailsService.java
+++ src/main/java/com/resumeApp/ResumeApp/services/PersonalDetailsService.java
@@ -2,7 +2,6 @@ package com.resumeApp.ResumeApp.services;
 
 import com.resumeApp.ResumeApp.exceptions.ResumeAppException;
 import com.resumeApp.ResumeApp.models.PersonalDetails;
-import com.resumeApp.ResumeApp.models.User;
 import com.resumeApp.ResumeApp.models.dtos.PersonalDetailsDTO;
 
 public interface PersonalDetailsService {
