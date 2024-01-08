4b4ec463bf4526f52f658d0fc02d40ef54597b25
Matej Thomka <matej.thomka.eleinst@gmail.com>
2024-01-08 22:23:50 +0100

    Data transfer for user model.

---
+++ src/main/java/com/resumeApp/ResumeApp/models/dtos/UserDTO.java
@@ -0,0 +1,24 @@
+package com.resumeApp.ResumeApp.models.dtos;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+
+@Builder
+@Getter
+@Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDTO {
+
+  String name;
+  String lastname;
+  String email;
+  String phoneNumber;
+  String address;
+}
