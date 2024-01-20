4012fe4a0559e8db48277cb197b1b9689eda7c88
Matej Thomka <matej.thomka.eleinst@gmail.com>
2024-01-20 01:23:14 +0100

    Create authentication manager.

--- src/main/java/com/mth/resume_app/security/WebSecurityConfig.java
+++ src/main/java/com/mth/resume_app/security/WebSecurityConfig.java
@@ -1,8 +1,11 @@
 package com.mth.resume_app.security;
 
+import com.mth.resume_app.services.CustomUserDetailsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -11,6 +14,19 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class WebSecurityConfig {
+
+  private final CustomUserDetailsService userDetailsService;
+  private final JwtAuthorizationFilter jwtAuthorizationFilter;
+
+  @Bean
+  public AuthenticationManager authenticationManager(HttpSecurity httpSecurity,
+                                                     PasswordEncoder bcrypt)
+    throws Exception {
+    AuthenticationManagerBuilder authenticationManagerBuilder = httpSecurity.
+        getSharedObject(AuthenticationManagerBuilder.class);
+    authenticationManagerBuilder.userDetailsService(userDetailsService).passwordEncoder(bcrypt);
+    return authenticationManagerBuilder.build();
+  }
 
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
