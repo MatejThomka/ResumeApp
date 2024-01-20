fe466cf994f97e711f06cc8fd13d95f5fa0c2074
Matej Thomka <matej.thomka.eleinst@gmail.com>
2024-01-20 01:18:53 +0100

    Create internal filter for jwt token with logic.

---
+++ src/main/java/com/mth/resume_app/security/JwtAuthorizationFilter.java
@@ -0,0 +1,68 @@
+package com.mth.resume_app.security;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.Claims;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthorizationFilter extends OncePerRequestFilter {
+
+  private final JwtUtil jwtUtil;
+  private final ObjectMapper objectMapper;
+
+  @Override
+  protected void doFilterInternal(@NonNull HttpServletRequest request,
+                                  @NonNull HttpServletResponse response,
+                                  @NonNull FilterChain filterChain)
+      throws ServletException, IOException {
+
+    Map<String, Object> errorDetails = new HashMap<>();
+
+    try {
+      String accessToken = jwtUtil.resolveToken(request);
+
+      if (accessToken == null) {
+        filterChain.doFilter(request, response);
+        return;
+      }
+
+      if (jwtUtil.isBlackListed(accessToken)){
+        return;
+      }
+
+      Claims claims = jwtUtil.resolveClaims(request);
+
+      if (claims != null && jwtUtil.validateClaims(claims)) {
+        String email = claims.getSubject();
+        Authentication authentication = new UsernamePasswordAuthenticationToken(email, "", new ArrayList<>());
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+      }
+    } catch (Exception e) {
+      errorDetails.put("message", "Authentication Error");
+      errorDetails.put("details", e.getMessage());
+      response.setStatus(HttpStatus.FORBIDDEN.value());
+      response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+
+      objectMapper.writeValue(response.getWriter(), errorDetails);
+    }
+
+    filterChain.doFilter(request, response);
+  }
+}
