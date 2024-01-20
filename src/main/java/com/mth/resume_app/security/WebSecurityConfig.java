89d806e201ea9cb3b57d1e338caf717631cccbf1
Matej Thomka <matej.thomka.eleinst@gmail.com>
2024-01-20 01:24:39 +0100

    Add BCryptPasswordEncoder.

--- src/main/java/com/mth/resume_app/security/WebSecurityConfig.java
+++ src/main/java/com/mth/resume_app/security/WebSecurityConfig.java
@@ -8,7 +8,10 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -41,5 +44,10 @@ public class WebSecurityConfig {
     httpSecurity.addFilterBefore(jwtAuthorizationFilter, UsernamePasswordAuthenticationFilter.class);
 
     return httpSecurity.build();
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
   }
 }
