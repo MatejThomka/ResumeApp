9f34cece43b24965b7be145514ec6a0a4b82ea5f
Matej Thomka <matej.thomka.eleinst@gmail.com>
2024-01-20 01:06:42 +0100

    Change AuthenticationResponseDTO to AuthenticationDTO.

--- src/main/java/com/mth/resume_app/controllers/AuthController.java
+++ src/main/java/com/mth/resume_app/controllers/AuthController.java
@@ -1,6 +1,7 @@
 package com.mth.resume_app.controllers;
 
 import com.mth.resume_app.exceptions.ResumeAppException;
+import com.mth.resume_app.models.dtos.AuthenticationDTO;
 import com.mth.resume_app.models.dtos.LoginDTO;
 import com.mth.resume_app.models.dtos.RegisterDTO;
 import com.mth.resume_app.services.AuthService;
@@ -21,21 +22,24 @@ public class AuthController {
 
   @PostMapping("/register")
   public ResponseEntity<?> register(@RequestBody RegisterDTO registerDTO) {
+    AuthenticationDTO authenticationDTO;
 
     try {
-      authService.register(registerDTO);
+      authenticationDTO = authService.register(registerDTO);
     } catch (ResumeAppException e) {
       return new ResponseEntity<>(e.getMessage(), HttpStatus.CONFLICT);
     }
 
-    return new ResponseEntity<>("Registration successfully!", HttpStatus.CREATED);
+    return new ResponseEntity<>(authenticationDTO, HttpStatus.CREATED);
   }
 
   @PostMapping("/login")
   public ResponseEntity<?> login(@RequestBody LoginDTO loginDTO) {
 
+    AuthenticationDTO authenticationDTO;
+
     try {
-      authService.login(loginDTO);
+      authenticationDTO = authService.login(loginDTO);
     } catch (ResumeAppException e) {
       return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
     }
