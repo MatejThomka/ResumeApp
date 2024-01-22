468e7de30bd5c7a3289f5b97acc91a52ba223fee
Matej Thomka <matej.thomka.eleinst@gmail.com>
2024-01-22 02:11:16 +0100

    Create service class with implementation of user service. Add logic for credentials and updateCredentials methods.

---
+++ src/main/java/com/mth/resume_app/services/UserServiceImpl.java
@@ -0,0 +1,56 @@
+package com.mth.resume_app.services;
+
+import com.mth.resume_app.exceptions.ResumeAppException;
+import com.mth.resume_app.exceptions.UserNotFoundException;
+import com.mth.resume_app.models.User;
+import com.mth.resume_app.models.dtos.UserDTO;
+import com.mth.resume_app.repositories.UserRepository;
+import com.mth.resume_app.security.JwtUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+
+    private final JwtUtil jwtUtil;
+    private final UserRepository userRepository;
+    @Override
+    public UserDTO credentials() throws ResumeAppException {
+        User user;
+
+        user = userRepository.findByEmail(jwtUtil.getEmail()).orElseThrow(() -> new UserNotFoundException("User not found!"));
+
+        return UserDTO.builder()
+                .name(user.getName())
+                .lastname(user.getLastname())
+                .email(user.getEmail())
+                .phoneNumber(user.getPhoneNumber())
+                .address(user.getAddress())
+                .build();
+    }
+
+    @Override
+    public UserDTO updateCredentials(User user) throws ResumeAppException {
+        User updatedUser;
+
+        updatedUser = userRepository.findByEmail(jwtUtil.getEmail()).orElseThrow(() -> new UserNotFoundException("User not found"));
+
+        if (user.getName() != null) updatedUser.setName(user.getName());
+        if (user.getLastname() != null) updatedUser.setLastname(user.getLastname());
+        if (user.getAddress() != null) updatedUser.setAddress(user.getAddress());
+        if (user.getPhoneNumber() != null) updatedUser.setPhoneNumber(user.getPhoneNumber());
+
+        userRepository.save(updatedUser);
+
+        return UserDTO.builder()
+                .name(updatedUser.getName())
+                .lastname(updatedUser.getLastname())
+                .email(updatedUser.getEmail())
+                .address(updatedUser.getAddress())
+                .phoneNumber(updatedUser.getPhoneNumber())
+                .build();
+    }
+
+
+}
