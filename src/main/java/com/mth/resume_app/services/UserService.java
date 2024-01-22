6507be348d305898fef2b6ecb7b24043ce9a1e6b
Matej Thomka <matej.thomka.eleinst@gmail.com>
2024-01-22 16:30:42 +0100

    Add methods for email change and delete account.

--- src/main/java/com/mth/resume_app/services/UserService.java
+++ src/main/java/com/mth/resume_app/services/UserService.java
@@ -1,6 +1,7 @@
 package com.mth.resume_app.services;
 
 import com.mth.resume_app.exceptions.ResumeAppException;
+import com.mth.resume_app.models.dtos.EmailDTO;
 import com.mth.resume_app.models.dtos.PasswordDTO;
 import com.mth.resume_app.models.dtos.UserDTO;
 
@@ -9,4 +10,6 @@ public interface UserService {
     UserDTO credentials() throws ResumeAppException;
     UserDTO updateCredentials(UserDTO userDTO) throws ResumeAppException;
     void passwordChange(PasswordDTO passwordDTO) throws ResumeAppException;
+    void emailChange(EmailDTO emailDTO) throws ResumeAppException;
+    void deleteAccount(String email, PasswordDTO passwordDTO) throws ResumeAppException;
 }
