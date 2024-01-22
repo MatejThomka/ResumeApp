1460cd41d1f59298cb487e75b17f1cd30c595e5a
Matej Thomka <matej.thomka.eleinst@gmail.com>
2024-01-22 02:09:05 +0100

    Add authority settings for enabling roles.

--- src/main/java/com/mth/resume_app/security/JwtAuthorizationFilter.java
+++ src/main/java/com/mth/resume_app/security/JwtAuthorizationFilter.java
@@ -7,15 +7,15 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -51,7 +51,13 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
 
       if (claims != null && jwtUtil.validateClaims(claims)) {
         String email = claims.getSubject();
-        Authentication authentication = new UsernamePasswordAuthenticationToken(email, "", new ArrayList<>());
+
+        String role = claims.get("roles", String.class);
+
+        List<GrantedAuthority> authorities = Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + role));
+
+        Authentication authentication = new UsernamePasswordAuthenticationToken(email, "", authorities);
+
         SecurityContextHolder.getContext().setAuthentication(authentication);
       }
     } catch (Exception e) {
