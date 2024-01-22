9565db48f6645383a2e9f8d0d9e0fa003a3cad56
Matej Thomka <matej.thomka.eleinst@gmail.com>
2024-01-22 14:37:45 +0100

    Add method for password changing with exception.

--- src/main/java/com/mth/resume_app/services/UserService.java
+++ src/main/java/com/mth/resume_app/services/UserService.java
@@ -1,11 +1,12 @@
 package com.mth.resume_app.services;
 
 import com.mth.resume_app.exceptions.ResumeAppException;
-import com.mth.resume_app.models.User;
+import com.mth.resume_app.models.dtos.PasswordDTO;
 import com.mth.resume_app.models.dtos.UserDTO;
 
 public interface UserService {
 
     UserDTO credentials() throws ResumeAppException;
-    UserDTO updateCredentials(User user) throws ResumeAppException;
+    UserDTO updateCredentials(UserDTO userDTO) throws ResumeAppException;
+    void passwordChange(PasswordDTO passwordDTO) throws ResumeAppException;
 }
