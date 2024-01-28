cc6c8f1007f926ab1e48d51bef3264c28a3303f3
Matej Thomka <matej.thomka.eleinst@gmail.com>
2024-01-28 03:58:26 +0100

    Delete unused import. Refactor methods.

--- src/main/java/com/mth/resume_app/services/PersonalDetailsService.java
+++ src/main/java/com/mth/resume_app/services/PersonalDetailsService.java
@@ -1,12 +1,11 @@
 package com.mth.resume_app.services;
 
 import com.mth.resume_app.exceptions.ResumeAppException;
-import com.mth.resume_app.models.PersonalDetails;
 import com.mth.resume_app.models.dtos.PersonalDetailsDTO;
 
 
 public interface PersonalDetailsService {
 
-  PersonalDetailsDTO showPersonalDetails(String email) throws ResumeAppException;
-  void createPersonalDetails(String email, PersonalDetails personalDetails) throws ResumeAppException;
+  PersonalDetailsDTO showPersonalDetails() throws ResumeAppException;
+  PersonalDetailsDTO createUpdatePersonalDetails(PersonalDetailsDTO personalDetailsDTO) throws ResumeAppException;
 }
