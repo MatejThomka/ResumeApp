a82633fe20eb81605c23250104abc450560fea3a
Matej Thomka <matej.thomka.eleinst@gmail.com>
2024-01-31 20:24:15 +0100

    Add method to find user via username.

--- src/main/java/com/mth/resume_app/repositories/UserRepository.java
+++ src/main/java/com/mth/resume_app/repositories/UserRepository.java
@@ -9,5 +9,6 @@ import org.springframework.stereotype.Repository;
 public interface UserRepository extends JpaRepository<User, Integer> {
 
   Optional<User> findByEmail(String email);
+  Optional<User> findByUsername(String username);
 
 }
