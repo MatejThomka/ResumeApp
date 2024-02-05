3cf594819e902a9cfb557de189afe495c04c4795
Matej Thomka <matej.thomka.eleinst@gmail.com>
2024-02-05 05:01:33 +0100

    Refactor name of controller from PersonalDetailsController to DetailsController.

--- src/main/java/com/mth/resume_app/controllers/DetailsController.java
+++ src/main/java/com/mth/resume_app/controllers/DetailsController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/personal-details")
-public class PersonalDetailsController {
+public class DetailsController {
 
   private final PersonalDetailsService personalDetailsService;
 
