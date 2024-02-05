324464291942a2e978899f4a6dd082b762f776ae
Matej Thomka <matej.thomka.eleinst@gmail.com>
2024-02-05 05:03:28 +0100

    Create exception for education.

---
+++ src/main/java/com/mth/resume_app/exceptions/EducationException.java
@@ -0,0 +1,7 @@
+package com.mth.resume_app.exceptions;
+
+public class EducationException extends ResumeAppException {
+    public EducationException(String message) {
+        super(message);
+    }
+}
