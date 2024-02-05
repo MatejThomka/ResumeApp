3b5fb70823599d68b908ffef8c7828a3a4f048a9
Matej Thomka <matej.thomka.eleinst@gmail.com>
2024-02-05 05:19:31 +0100

    Add description for all methods.

--- src/main/java/com/mth/resume_app/services/EducationServiceImpl.java
+++ src/main/java/com/mth/resume_app/services/EducationServiceImpl.java
@@ -24,6 +24,18 @@ public class EducationServiceImpl implements EducationService {
 
     private final EducationRepository educationRepository;
     private final UserExtraction extraction;
+
+    /**
+     * Creates or updates High School education details for the specified user.
+     * Validates the year in the provided EducationDTO, retrieves the user by username,
+     * and fetches or initializes the High School entity. The method updates attributes such as
+     * school-leaving exam status in the High School entity and calls the common saveEducation
+     * method to persist the changes.
+     *
+     * @param username     The username of the user for whom education details are being updated.
+     * @param educationDTO The EducationDTO containing information about the High School education.
+     * @throws ResumeAppException If validation fails or user information is not found.
+     */
     @Override
     public void cOUHighSchool(String username, EducationDTO educationDTO) throws ResumeAppException {
         isYearValid(educationDTO);
@@ -38,6 +50,17 @@ public class EducationServiceImpl implements EducationService {
         saveEducation(highSchool, educationDTO);
     }
 
+    /**
+     * Creates or updates University education details for the specified user.
+     * Validates the year in the provided EducationDTO, retrieves the user by username,
+     * and fetches or initializes the University entity. The method updates attributes such as
+     * faculty in the University entity and calls the common saveEducation
+     * method to persist the changes.
+     *
+     * @param username     The username of the user for whom education details are being updated.
+     * @param educationDTO The EducationDTO containing information about the University education.
+     * @throws ResumeAppException If validation fails or user information is not found.
+     */
     @Override
     public void cOUUniversity(String username, EducationDTO educationDTO) throws ResumeAppException {
         isYearValid(educationDTO);
@@ -52,6 +75,17 @@ public class EducationServiceImpl implements EducationService {
         saveEducation(university, educationDTO);
     }
 
+    /**
+     * Creates or updates CourseOrCertificate education details for the specified user.
+     * Validates the year in the provided EducationDTO, retrieves the user by username,
+     * and fetches or initializes the CourseOrCertificate entity. The method updates attributes such as
+     * nameOfInstitution in the CourseOrCertificate entity and calls the common saveEducation
+     * method to persist the changes.
+     *
+     * @param username     The username of the user for whom education details are being updated.
+     * @param educationDTO The EducationDTO containing information about the CourseOrCertificate education.
+     * @throws ResumeAppException If validation fails or user information is not found.
+     */
     @Override
     public void cOUCourseOrCertificate(String username, EducationDTO educationDTO) throws ResumeAppException {
         isYearValid(educationDTO);
@@ -66,6 +100,15 @@ public class EducationServiceImpl implements EducationService {
         saveEducation(courseOrCertificate, educationDTO);
     }
 
+    /**
+     * Retrieves and returns a list of EducationDTOs representing education details for the specified user.
+     * It fetches the user by username, then retrieves all education entities associated with that user.
+     * The method maps each education entity to its corresponding EducationDTO and collects them into a list.
+     *
+     * @param username The username of the user for whom education details are being retrieved.
+     * @return A list of EducationDTOs representing the user's education details.
+     * @throws ResumeAppException If user information is not found.
+     */
     @Override
     public List<EducationDTO> show(String username) throws ResumeAppException {
 
@@ -77,6 +120,15 @@ public class EducationServiceImpl implements EducationService {
                 .collect(Collectors.toList());
     }
 
+    /**
+     * Deletes the education entry with the specified ID associated with the given username.
+     * It fetches the user by username and then retrieves the education entity based on the provided ID.
+     * If the education entry is found, it is deleted from the repository.
+     *
+     * @param username The username of the user associated with the education entry.
+     * @param id       The ID of the education entry to be deleted.
+     * @throws ResumeAppException If the education entry is not found.
+     */
     @Override
     public void delete(String username, Integer id) throws ResumeAppException {
 
@@ -87,6 +139,15 @@ public class EducationServiceImpl implements EducationService {
         educationRepository.delete(education);
     }
 
+    /**
+     * Saves the education entity based on the provided EducationDTO.
+     * It takes an existing or new education entity and updates its attributes such as 'name,' 'fieldOfStudy,'
+     * 'schoolLeavingExam,' 'studying,' 'yearTill,' 'city,' 'yearFrom,' and 'description' based on the
+     * corresponding attributes in the EducationDTO. The method then saves the changes to the repository.
+     *
+     * @param education     The existing or new education entity to be updated or created.
+     * @param educationDTO  The EducationDTO containing the updated or new education details.
+     */
     private void saveEducation(Education education, EducationDTO educationDTO) {
 
         education.setName(educationDTO.getName());
@@ -111,6 +172,14 @@ public class EducationServiceImpl implements EducationService {
         educationRepository.save(education);
     }
 
+    /**
+     * Validates the years in the provided EducationDTO.
+     * It checks if the 'yearFrom' and 'yearTill' are within a valid range, ensuring they are not earlier
+     * than 1950, not higher than the current year, and that 'yearTill' is not earlier than 'yearFrom.'
+     *
+     * @param educationDTO The EducationDTO containing the years to be validated.
+     * @throws YearException If the years are not within a valid range or 'yearTill' is earlier than 'yearFrom.'
+     */
     private void isYearValid(EducationDTO educationDTO) throws ResumeAppException {
         if (educationDTO.getYearFrom() < 1950 || educationDTO.getYearTill() < 1950) {
             throw new YearException("Year is too low!");
