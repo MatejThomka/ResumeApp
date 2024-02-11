d1d5cd60ab8302667153dda3a699b8f25f31910d
Matej Thomka <matej.thomka.eleinst@gmail.com>
2024-02-11 20:37:42 +0100

    Create exception for experience.

---
+++ src/main/java/com/mth/resume_app/exceptions/ExperienceException.java
@@ -0,0 +1,7 @@
+package com.mth.resume_app.exceptions;
+
+public class ExperienceException extends ResumeAppException {
+    public ExperienceException(String message) {
+        super(message);
+    }
+}
