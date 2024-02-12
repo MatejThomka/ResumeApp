42f7ff53fa999489995e772b2dc0ec0d465809b7
Matej Thomka <matej.thomka.eleinst@gmail.com>
2024-02-12 01:47:28 +0100

    Add path to info into security config.

--- src/main/java/com/mth/resume_app/security/WebSecurityConfig.java
+++ src/main/java/com/mth/resume_app/security/WebSecurityConfig.java
@@ -45,6 +45,7 @@ public class WebSecurityConfig {
                     .requestMatchers("/api/education/{username}/**").hasAnyRole("USER", "ADMIN", "COMPANY")
                     .requestMatchers("/api/skill/{username}/**").hasAnyRole("USER", "ADMIN", "COMPANY")
                     .requestMatchers("/api/experience/{username}/**").hasAnyRole("USER", "ADMIN", "COMPANY")
+                    .requestMatchers("/api/info/{username}/**").hasAnyRole("USER", "ADMIN", "COMPANY")
                     .anyRequest().authenticated()
         )
             .exceptionHandling(handling -> handling.accessDeniedHandler(customAccessDeniedHandler));
