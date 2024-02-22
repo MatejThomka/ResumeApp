fff6b0348734816c4195df80a2f9320a532769ca
Matej Thomka <matej.thomka.eleinst@gmail.com>
2024-02-22 17:56:52 +0100

    Add imports, create constructor, set background.

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -1,13 +1,27 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {AfterViewInit, Component, ElementRef,} from '@angular/core';
+import {RouterLink, RouterOutlet} from '@angular/router';
+import {GeneralService} from "./services/general.service";
+import {NgIf} from "@angular/common";
+import {LoginDialogComponent} from "./components/login-dialog/login-dialog.component";
+import {RegisterDialogComponent} from "./components/register-dialog/register-dialog.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, NgIf, LoginDialogComponent, RegisterDialogComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'ui';
+export class AppComponent implements AfterViewInit{
+  title = 'ResumeAppUI';
+
+  constructor(
+    public generalService: GeneralService,
+    private elementRef: ElementRef
+  ) {  }
+
+  ngAfterViewInit() {
+    this.elementRef.nativeElement.ownerDocument
+      .body.style.background = 'rgba(85,231,214,0.29)'
+  }
 }
