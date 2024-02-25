49d854bc1f70e9bb8a648d8b096e2f296c502794
Matej Thomka <matej.thomka.eleinst@gmail.com>
2024-02-25 17:55:27 +0100

    Add logic for login and message handling.

--- ui/src/app/components/login-dialog/login-dialog.component.ts
+++ ui/src/app/components/login-dialog/login-dialog.component.ts
@@ -1,16 +1,59 @@
 import {Component, OnInit} from '@angular/core';
 import {GeneralService} from "../../services/general.service";
+import {AuthService} from "../../services/auth.service";
+import {FormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-login-dialog',
-  standalone: true,
-  imports: [],
   templateUrl: './login-dialog.component.html',
+  standalone: true,
+  imports: [
+    FormsModule,
+    HttpClientModule,
+    NgIf
+  ],
   styleUrl: '../dialog.component.scss'
 })
 export class LoginDialogComponent implements OnInit {
-  constructor(public generalService: GeneralService) { }
+
+  email: string = '';
+  password: string = '';
+  message: string = '';
+  messageType: string = '';
+
+  constructor(
+    public generalService: GeneralService,
+    public authService: AuthService
+  ) { }
 
   ngOnInit() {
+  }
+
+  login() {
+    this.authService.login(this.email, this.password).subscribe(
+      (response) => {
+        this.authService.saveToken(response.token);
+        this.authService.saveUsername(response.username);
+        this.message = 'Login successfully!'
+        this.messageType = 'success';
+        setTimeout(() =>
+          {
+            this.message = '';
+            this.generalService.showLoginDialog = false;
+          }, 2000);
+      },
+      (error) => {
+        console.error('Register error', error);
+        if (error.status === 403) {
+          this.message = 'Incorrect credentials!';
+          this.messageType = 'error';
+          setTimeout(() => {
+            this.message = '';
+          }, 5000);
+        }
+      }
+    )
   }
 }
