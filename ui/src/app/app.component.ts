ce5882a69552689b3ad0ebbe840b442e53ddfa31
Matej Thomka <matej.thomka.eleinst@gmail.com>
2024-02-25 17:52:10 +0100

    Add new imports.

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -1,15 +1,21 @@
 import {AfterViewInit, Component, ElementRef,} from '@angular/core';
-import {RouterLink, RouterOutlet} from '@angular/router';
 import {GeneralService} from "./services/general.service";
-import {NgIf} from "@angular/common";
-import {LoginDialogComponent} from "./components/login-dialog/login-dialog.component";
 import {RegisterDialogComponent} from "./components/register-dialog/register-dialog.component";
+import {LoginDialogComponent} from "./components/login-dialog/login-dialog.component";
+import {NgIf} from "@angular/common";
+import {HttpClientModule} from "@angular/common/http";
+import {AuthService} from "./services/auth.service";
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, RouterLink, NgIf, LoginDialogComponent, RegisterDialogComponent],
   templateUrl: './app.component.html',
+  standalone: true,
+  imports: [
+    RegisterDialogComponent,
+    LoginDialogComponent,
+    NgIf,
+    HttpClientModule
+  ],
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements AfterViewInit{
