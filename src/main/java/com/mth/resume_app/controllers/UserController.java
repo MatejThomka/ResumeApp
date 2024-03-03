7297a9b5d1c7ea1b5e77fce7570cbb80b45482c5
Matej Thomka <matej.thomka.eleinst@gmail.com>
2024-03-03 22:20:27 +0100

    Add CORS annotation for frontend address.

--- src/main/java/com/mth/resume_app/controllers/UserController.java
+++ src/main/java/com/mth/resume_app/controllers/UserController.java
@@ -14,6 +14,7 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/user")
+@CrossOrigin(origins = "http://localhost:4200/")
 public class UserController {
 
     private final UserService userService;
