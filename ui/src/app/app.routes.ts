f21080af70bc3b5e259791b542bc94e55d98b0ae
Matej Thomka <matej.thomka.eleinst@gmail.com>
2024-04-02 20:32:55 +0200

    Add route for details.

--- ui/src/app/app.routes.ts
+++ ui/src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import {UserComponent} from "./components/user/user.component";
 import {HomeComponent} from "./components/home/home.component";
+import {DetailsComponent} from "./components/details/details.component";
 
 export const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: 'user/:username', component: UserComponent},
   {path: 'home/:username', component: HomeComponent},
-  {path: 'home', component: HomeComponent}
+  {path: 'home', component: HomeComponent},
+  {path: 'details/:username', component: DetailsComponent}
 ];
