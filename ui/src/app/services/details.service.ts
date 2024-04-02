b0b23a0988861d1f3d94a61aa0e11304e40c7717
Matej Thomka <matej.thomka.eleinst@gmail.com>
2024-04-02 20:37:12 +0200

    Create service for details with logic to communicate with API part.

---
+++ ui/src/app/services/details.service.ts
@@ -0,0 +1,54 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DetailsService {
+
+  private detailsApiUrl = 'http://localhost:6868/api/personal-details'
+  constructor(private http: HttpClient) { }
+
+  personalDetails(token: string,
+                  username: string): Observable<any> {
+    return this.http.get<any>(`${this.detailsApiUrl}/${username}`, {
+      headers: {
+        'Authorization': `Bearer ${token}`
+      }
+    })
+  }
+
+  updateDetails(token: string,
+                username: string,
+                dateOfBirth: string,
+                placeOfBirth: string,
+                city: string,
+                streetAndNumber: string,
+                postalCode: string,
+                country: string,
+                gender: string,
+                drivingLicence: boolean,
+                drivingGroups: string[])
+  {
+    return this.http.patch<any>(
+      `${this.detailsApiUrl}/${username}/create-update`,
+      {
+        dateOfBirth,
+        placeOfBirth,
+        city,
+        streetAndNumber,
+        postalCode,
+        country,
+        gender,
+        drivingLicence,
+        drivingGroups
+      },
+      {
+        headers: {
+          'Authorization': `Bearer ${token}`
+        }
+      }
+    )
+  }
+}
