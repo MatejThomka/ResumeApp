828cdc0afe854795e53bc69973a16be6d9e1b333
Matej Thomka <matej.thomka.eleinst@gmail.com>
2024-04-02 20:34:04 +0200

    Add origin for  cross-origin settings.

--- src/main/java/com/mth/resume_app/controllers/DetailsController.java
+++ src/main/java/com/mth/resume_app/controllers/DetailsController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/personal-details")
+@CrossOrigin(origins = "http://localhost:4200/")
 public class DetailsController {
 
   private final PersonalDetailsService personalDetailsService;
