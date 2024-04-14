de094c974d33ee0d71549c77ca49f3c39d9ba9e7
Matej Thomka <matej.thomka.eleinst@gmail.com>
2024-04-14 17:46:01 +0200

    add path for details and education.

--- ui/src/app/app.routes.ts
+++ ui/src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import {UserComponent} from "./components/user/user.component";
 import {HomeComponent} from "./components/home/home.component";
 import {DetailsComponent} from "./components/details/details.component";
+import {EducationComponent} from "./components/education/education.component";
 
 export const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: 'user/:username', component: UserComponent},
   {path: 'home/:username', component: HomeComponent},
   {path: 'home', component: HomeComponent},
-  {path: 'details/:username', component: DetailsComponent}
+  {path: 'details/:username', component: DetailsComponent},
+  {path: 'education/:username', component: EducationComponent}
 ];
