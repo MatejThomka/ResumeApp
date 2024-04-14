bae18a9566d192f9b7f9149b8f1dd81cf20f0ef8
Matej Thomka <matej.thomka.eleinst@gmail.com>
2024-04-14 17:49:45 +0200

    Delete unused import.

--- ui/src/app/services/education.service.ts
+++ ui/src/app/services/education.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
-import {routes} from "../app.routes";
 import {Education} from "../interfaces/education";
 
 @Injectable({
