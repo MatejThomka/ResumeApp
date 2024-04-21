6e3792b5ac8fa8fadb53e134d0af550e0624ced3
Matej Thomka <matej.thomka.eleinst@gmail.com>
2024-04-21 17:46:36 +0200

    Refactor data for handling if it is in model or not.

--- ui/src/app/interfaces/education.ts
+++ ui/src/app/interfaces/education.ts
@@ -1,15 +1,15 @@
 export interface Education {
 
   id?: number;
-  educationType: string | null;
-  nameOfInstitution: string;
-  faculty: string;
-  name: string;
-  fieldOfStudy: string;
-  city: string;
-  schoolLeavingExam: boolean;
-  yearFrom: number | null;
-  isStudying: boolean;
-  yearTill: number | null;
-  description: string;
+  educationType?: string | null;
+  nameOfInstitution?: string;
+  faculty?: string;
+  name?: string;
+  fieldOfStudy?: string;
+  city?: string;
+  schoolLeavingExam?: boolean;
+  yearFrom?: number | null;
+  isStudying?: boolean;
+  yearTill?: number | null;
+  description?: string;
 }
