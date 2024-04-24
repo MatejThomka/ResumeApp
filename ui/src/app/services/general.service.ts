4407e7d62c18da7040070d80b134717c69c3cc69
Matej Thomka <matej.thomka.eleinst@gmail.com>
2024-04-25 00:54:26 +0200

    Add variable showDeleteConfirmationDialog boolean.

--- ui/src/app/services/general.service.ts
+++ ui/src/app/services/general.service.ts
@@ -6,5 +6,6 @@ import { Injectable } from '@angular/core';
 export class GeneralService {
   showLoginDialog = false;
   showRegisterDialog = false;
+  showDeleteConfirmationDialog = false;
   constructor() { }
 }
