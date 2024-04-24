54252a2149239032006697be705c5aa1400ceb77
Matej Thomka <matej.thomka.eleinst@gmail.com>
2024-04-24 05:17:19 +0200

    Refactor some data types and delete safe mark.

--- ui/src/app/interfaces/education.ts
+++ ui/src/app/interfaces/education.ts
@@ -1,15 +1,15 @@
 export interface Education {
 
   id?: number;
-  educationType?: string | null;
-  nameOfInstitution?: string;
-  faculty?: string;
-  name?: string;
-  fieldOfStudy?: string;
-  city?: string;
-  schoolLeavingExam?: boolean;
-  yearFrom?: number | null;
-  isStudying?: boolean;
-  yearTill?: number | null;
-  description?: string;
+  name: string;
+  educationType: string;
+  fieldOfStudy: string;
+  yearFrom: number;
+  yearTill?: number;
+  description: string;
+  studying: boolean;
+  city: string;
+  schoolLeavingExam: boolean;
+  faculty: string;
+  nameOfInstitution: string;
 }
